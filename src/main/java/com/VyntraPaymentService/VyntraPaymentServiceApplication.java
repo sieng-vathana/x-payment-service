de31a7e075985d34ea67253f8301de5a38dfa41b
Vathana Programmer <vathana@example.com>
2026-05-29 19:17:44 +0700

    Initial commit

---
+++ src/main/java/com/VyntraPaymentService/VyntraPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.VyntraPaymentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyntraPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyntraPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VyntraPaymentService/VyntraPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VyntraPaymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VyntraPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
